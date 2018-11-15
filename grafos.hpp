bc127df93e5575a38c3c0b16afcbaa70ca522ecc
Hugo Sales <hdcs333@gmail.com>
2018-11-15 00:10:56 +0000

    Initial port of graphs

--- grafos.hpp
+++ grafos.hpp
@@ -1,7 +1,23 @@
 #include <tr1/unordered_map>
 #include <cstddef>
-#include <vector>
+#include <list>
 #include <limits>
+#include <algorithm>
+
+#define NUM_VERTICES(g) ( (g) ->vertex_count() )
+// numero de vertices
+#define NUM_ARCOS(g) ( (g) -> narcos )
+// numero de arcos
+#define ADJS_NO(i,g) ( (g) -> verts[i].adjs )
+// primeiro arco da lista de adjacentes do nó i
+#define PROX_ADJ(arco) ((arco) -> prox)
+// proximo adjacente
+#define ADJ_VALIDO(arco) (((arco) != NULL))
+// se arco é válido
+#define EXTREMO_FINAL(arco) ((arco) -> no_final)
+// qual o extremo final de arco
+#define VALOR_ARCO(arco) ((arco) -> valor)
+// qual o valor do arco
 
 // ---------------------------------------------------------
 // More modern C++ features in C++98
@@ -18,43 +34,77 @@ template<class Value, class Index = Value>
 struct arc {
 	Index final;
 	Value value;
-	arc<Value, Index> *next;
+	// arc<Value, Index> *next;
 	arc(Value value, Index final);
 };
 
 template<class Value, class Index = Value>
 struct node {
-	std::vector<arc<Value, Index> > adjs;
+	std::list<arc<Value, Index> > adjs;
 };
 
 template<class Value, class Index = Value>
 struct graph : std::tr1::unordered_map<Index, node<Value, Index> > {
+	size_t n_arcs;
+
 	// Index is assumed to be an integral value. If this were at least C++11,
 	// we could use static asserts and type traits
 	// Enabled only if the type passed for index is an integer type. Not modern, but C++98
 	graph(size_t n,
-	      typename
-	      std_98::enable_if<std::numeric_limits<Index>::is_integer, void>::type* = nullptr) {
+	      typename // Unamed parameter used for SFINAE
+	      std_98::enable_if<std::numeric_limits<Index>::is_integer, void>::type* = NULL) {
 		this->reserve(n + 1);
+		n_arcs = 0;
 	}
 
 	/* insere arco (i, j) no grafo, bem como o seu peso; não evita repetições */
 	void insert_new_arc(Index i, Index j, Value value) {
 		/* insere arco (i,j) no grafo */
-		arc<Value, Index> arc = new arc{value, j};
-		arc.next = this->at(i).adjs;
-		this->at(i).adjs = arc;
+		++n_arcs;
+		this->at(i).adjs.push_front(new ::arc<Value, Index>(value, j));
 	}
 
-	/* retira adjacente arco da lista de adjacentes de i */
-	void remove_arc(Index i, Index j) {
-		this->at(i).adjs.at(j);
+	/* retira arco adjacente da lista de adjacentes de i */
+	bool remove_arc(Index i, Index j) {
+		std::list<arc<Index, Value> >& adjs = this->at(i).adjs;
+		for(typename std::list<arc<Index, Value> >::iterator &it = adjs.begin();
+		    it != adjs.end(); ++it) {
+			if(it->final == j) {
+				--n_arcs;
+				delete *it;
+				adjs.erase(it);
+				return true;
+			}
+		}
+		return false;
 	}
-	void remove_arc(Index i, arc<Value, Index> *arc);
+	bool remove_arc(Index i, arc<Value, Index> *arc) {
+		// ::arc because the parameter has the same name
+		std::list< ::arc<Value, Index> >& adjs = this->at(i).adjs;
+		for(typename std::list< ::arc<Index, Value> >::iterator &it = adjs.begin();
+		    it != adjs.end(); ++it) {
+			if(*it == arc) {
+				--n_arcs;
+				delete *it;
+				adjs.erase(it);
+				return true;
+			}
+		}
+		return true;
+	}
 
 	/* retorna um apontador para o arco (i, j) ou NULL se não existir */
-	arc<Value, Index> * find_arc(Index i, Index j);
+	arc<Value, Index> * find_arc(Index i, Index j) {
+		std::list< ::arc<Index, Value> >& adjs = this->at(i).adjs;
+		for(typename std::list<arc<Index, Value> >::iterator &it = adjs.begin();
+		    it != adjs.end(); ++it) {
+			if(*it->final == j) {
+				return *it;
+			}
+		}
+		return NULL;
+	}
 
-	size_t index_count() { return this->size(); }
-	std::vector<arc<Value, Index> > adjs_node(Index i) { return (*this)[i].adjs; }
+	size_t vertex_count() { return this->size(); }
+	std::list<arc<Value, Index> > adjs_node(Index i) { return this->at(i).adjs; }
 };
